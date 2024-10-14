bf7f5670b95f5a897d4fafc7f16da7a2b0c0bd25
sBrady28 <sbrady28@gmail.com>
2024-10-14 18:40:49 +0000

    Add Active Storage and validations to Student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,29 +1,34 @@
+
 class Student < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :major, presence: true
-  validates :expected_graduation_date, presence: true
-  validates :school_email, presence: true, uniqueness: true
-  validate :school_email_format
-  has_one_attached :profile_picture
-  validate :acceptable_image
 
-  def school_email_format
-    unless school_email =~ /\A[\w+\-.]+@msudenver\.edu\z/i
-      errors.add(:school_email, "must be an @msudenver.edu email address")
-    end
-  end
+   validates :first_name, presence: true
+   validates :last_name, presence: true
+   validates :major, presence: true
+   validates :expected_graduation_date, presence: true
+   validates :school_email, presence: true
+   validates :school_email, uniqueness: true
+   validate :school_email_format
+   has_one_attached :profile_picture
+   validate :acceptable_image
 
-  def acceptable_image
-    return unless profile_picture.attached?
+   def school_email_format
+       unless school_email =~ /\A[\w+\-.]+@msudenver\.edu\z/i
+           errors.add(:school_email, "must be an @msudenver.edu email address")
+       end
+   end
 
-    unless profile_picture.blob.byte_size <= 1.megabyte
-      errors.add(:profile_picture, "is too big")
-    end
+   def acceptable_image
+       return unless profile_picture.attached?
 
-    acceptable_types = ["image/jpeg", "image/png"]
-    unless acceptable_types.include?(profile_picture.content_type)
-      errors.add(:profile_picture, "must be a JPEG or PNG")
-    end
-  end
+       unless profile_picture.blob.byte_size <= 1.megabyte
+           errors.add(:profile_picture, "is too big")
+       end
+
+       acceptable_types = ["image/jpeg", "image/png"]
+       unless acceptable_types.include?(profile_picture.content_type)
+           errors.add(:profile_picture, "must be a JPEG or PNG")
+       end
+
+ end
+
 end
